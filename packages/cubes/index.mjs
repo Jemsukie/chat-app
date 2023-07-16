67d8a7f78d463284c0fc237d779735ab71fa9784
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-07-16 19:34:02 +0800

    feat(): try cron job in vercel

--- packages/cubes/index.mjs
+++ packages/cubes/index.mjs
@@ -1,21 +1,25 @@
+// cronJob.js
+
 import CubejsServer from '@cubejs-backend/server'
 
-const server = new CubejsServer({
-  http: {
-    cors: {
-      // TODO: from .env to dapat i-set sa production
-      origin: [process.env.CORS_ALLOWED_ORIGIN],
-      credentials: true,
+const cubejsCronJob = async () => {
+  const server = new CubejsServer({
+    http: {
+      cors: {
+        // TODO: Set this to the appropriate value in production
+        origin: [process.env.CORS_ALLOWED_ORIGIN],
+        credentials: true,
+      },
     },
-  },
-})
-
-server
-  .listen()
-  .then(({ version, port }) => {
-    console.log(`🚀 Cube.js server (${version}) is listening on ${port}`)
   })
-  .catch((e) => {
+
+  try {
+    const { version, port } = await server.listen()
+    console.log(`🚀 Cube.js server (${version}) is listening on ${port}`)
+  } catch (e) {
     console.error('Fatal error during server start: ')
     console.error(e.stack || e)
-  })
+  }
+}
+
+export default cubejsCronJob
